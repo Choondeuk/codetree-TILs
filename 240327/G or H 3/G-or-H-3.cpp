6869a050d1d35dac253ca6c696e209f8b1db5e8a
이영원 <82967985+Choondeuk@users.noreply.github.com>
2024-03-27 00:56:39 +0900

    Title: G or H 3, Time: 18ms, Memory: 0MB, Status: Passed - CodeTree

--- 240327/G or H 3/G-or-H-3.cpp
+++ 240327/G or H 3/G-or-H-3.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     int N, K;
     cin >> N >> K;
-    int max_score, score = 0;
+    int max_score = 0, score = 0;
     int placed[MAX] = {};
     char alp[MAX] = {};
     int map[MAX+1] = {};
@@ -13,9 +13,12 @@ int main() {
     }
     for(int i = 0; i < N; i++){
         if(alp[i] == 'G')   map[placed[i]] = 1;
-        else map[placed[i]] = 2;
+
+        else    map[placed[i]] = 2;
+
+
     }
-    for(int i = 1; i < MAX+1-K; i++){
+    for(int i = 1; i <= MAX+1-K; i++){
         for(int j = i; j < i+K+1; j++){
             score += map[j];
         }
