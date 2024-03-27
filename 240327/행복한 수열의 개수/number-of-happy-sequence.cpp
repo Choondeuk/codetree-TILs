19eb804a9f2de1bc760504a793bbc1bcae9eccf0
이영원 <82967985+Choondeuk@users.noreply.github.com>
2024-03-27 20:45:21 +0900

    Title: 행복한 수열의 개수, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240327/행복한 수열의 개수/number-of-happy-sequence.cpp
+++ 240327/행복한 수열의 개수/number-of-happy-sequence.cpp
@@ -19,7 +19,7 @@ int main() {
     }
     for(int col = 0; col < n; col++){
         prev = 0;
-        cnt_2 = 0;
+        cnt_2 = 1;
         for(int i = 1; i < n; i++){
             if(arr[col][prev] != arr[col][i]){
                 cnt_2  = 0; 
@@ -35,7 +35,7 @@ int main() {
 
     for(int row = 0; row < n; row++){
         prev = 0;
-        cnt_1 = 0;
+        cnt_1 = 1;
         for(int i = 1; i < n; i++){
             if(arr[prev][row] != arr[i][row]){
                 cnt_1 = 0;
