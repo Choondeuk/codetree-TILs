e0d1f9ae21c889696fc9d4197af09a3bbc7afcf8
이영원 <82967985+Choondeuk@users.noreply.github.com>
2024-03-29 17:09:07 +0900

    Title: 문자열 나누기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240329/문자열 나누기/divide-string.cpp
+++ 240329/문자열 나누기/divide-string.cpp
@@ -1,27 +1,34 @@
 #include <iostream>
 #include <string>
 using namespace std;
-int main() {
-    int n;
-    cin >> n;
-    string arr[10];
-    string temp = "";
-    for(int i = 0; i < n; i++){
-        cin >> arr[i];
-    }
-    for(int i = 0; i < n; i++){
-        temp += arr[i];
-    }
-    int cnt = 0;
-    for(int j = 0; j < temp.length(); j++){
-        cout << temp[j];
-        cnt++;
-        if(cnt == 5){
-            cout << endl;
-            cnt = 0;
-        }
 
-    }
-    // 여기에 코드를 작성해주세요.
+int main() {
+    // 문자열 배열을 구현합니다.
+	string str[10];
+	string str_all;
+	int n;
+	
+	// n을 입력받습니다.
+	cin >> n;
+	
+	// 공백 단위로 문자열을 입력받습니다.
+	for(int i = 0; i < n; i++)
+		cin >> str[i];
+	
+	// 문자열을 전부 붙입니다.
+	for(int i = 0; i < n; i++) {
+		str_all += str[i];
+	}
+	
+	// 합쳐진 문자열의 길이를 구합니다.
+	int len = str_all.length();
+	
+	// 합쳐진 문자열을 5개의 숫자마다 개행을 하여 출력합니다.
+	for(int i = 0; i < len; i++) {
+		cout << str_all[i];
+		if(i % 5 == 4)
+			cout << endl;
+	}
+	
     return 0;
 }
