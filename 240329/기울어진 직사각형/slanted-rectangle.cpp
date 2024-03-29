25706429907310df0a9b2d81cc0a8b2ce2efda8d
이영원 <82967985+Choondeuk@users.noreply.github.com>
2024-03-29 14:41:12 +0900

    Title: 기울어진 직사각형, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240329/기울어진 직사각형/slanted-rectangle.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+#define MAX 20
+#define NUM 4
+
+int arr[MAX][MAX] = {};
+int dx[NUM] = {1, -1, -1, 1};
+int dy[NUM] = {1, 1, -1, -1};
+int n;
+
+bool check_range(int y, int x){
+    return(0 <= x && x < n && y <= 0 && y < n);
+}
+int get_score(int y, int x, int k, int l){
+    int sum = 0;
+    int len[4] = {k, l, k, l};
+    for(int d = 0; d < NUM; d++){
+        for(int i = 0; i < len[d]; i++){
+            y += dy[d];
+            x += dx[d];
+            if(check_range){
+                sum += arr[y][x];
+            }
+        }
+    }
+    return sum;
+}
+int main(){
+
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            cin >> arr[i][j];
+        }
+    }
+    int max_score = 0;
+    for(int y = 0; y < n; y++){
+        for(int x = 0; x < n; x++){
+            for(int k = 0; k < 4; k++){
+                for(int l = 0; l < 4; l++){
+                    max_score = max(max_score,get_score(y, x, k, l));
+                }
+            }
+        }
+    }
+    cout << max_score;
+    return 0;
+}
