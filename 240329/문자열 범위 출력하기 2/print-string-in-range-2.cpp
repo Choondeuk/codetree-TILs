dc67544b93c6dda05b53a2941076fc959cc30c1e
이영원 <82967985+Choondeuk@users.noreply.github.com>
2024-03-29 16:11:15 +0900

    Title: 문자열 범위 출력하기 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240329/문자열 범위 출력하기 2/print-string-in-range-2.cpp
+++ 240329/문자열 범위 출력하기 2/print-string-in-range-2.cpp
@@ -7,10 +7,14 @@ int main() {
     cin >> str;
     int n;
     cin >> n;
+    int cnt = 0;
     int len = str.length();
-    for(int i = len - 1; i >= len - n; i--){
+    for(int i = len - 1; i >= 0; i--){
         cout << str[i];
+        cnt++;
+        if(cnt == n)    break;
     }
         // 여기에 코드를 작성해주세요.
+
     return 0;
 }
