63205148fba80d415f7bdf4f29cc050bb12faef0
이영원 <82967985+Choondeuk@users.noreply.github.com>
2024-03-29 14:49:36 +0900

    Title: 기울어진 직사각형, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240329/기울어진 직사각형/slanted-rectangle.cpp
+++ 240329/기울어진 직사각형/slanted-rectangle.cpp
@@ -12,14 +12,15 @@ bool check_range(int y, int x){
     return(0 <= x && x < n && 0 <= y && y < n);
 }
 int get_score(int y, int x, int k, int l){
-    int sum = 0;
+    
     int len[4] = {k, l, k, l};
     for(int d = 0; d < NUM; d++){
         for(int i = 0; i < len[d]; i++){
             y += dy[d];
             x += dx[d];
-            if(check_range(y, x)){
-                sum += arr[y][x];
+            if(check_range(y, x) == false)  return 0;
+            
+            sum += arr[y][x];
             }
         }
     }
@@ -36,8 +37,8 @@ int main(){
     int max_score = 0;
     for(int y = 0; y < n; y++){
         for(int x = 0; x < n; x++){
-            for(int k = 0; k < 4; k++){
-                for(int l = 0; l < 4; l++){
+            for(int k = 1; k < n; k++){
+                for(int l = 1; l < n; l++){
                     max_score = max(max_score, get_score(y, x, k, l));
                 }
             }
