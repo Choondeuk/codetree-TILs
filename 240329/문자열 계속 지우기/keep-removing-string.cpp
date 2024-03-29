e258fa4b98b3855521c87057e2dafc59328d3a64
이영원 <82967985+Choondeuk@users.noreply.github.com>
2024-03-29 21:04:09 +0900

    Title: 문자열 계속 지우기, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240329/문자열 계속 지우기/keep-removing-string.cpp
+++ 240329/문자열 계속 지우기/keep-removing-string.cpp
@@ -4,11 +4,12 @@ using namespace std;
 int main() {
     string a, b;
     cin >> a >> b;
-    int a_len = a.length();
-    int b_len = b.length();
+
     
     while(1){
         bool exist = false;
+        int a_len = a.length();
+        int b_len = b.length();
         for(int i = 0; i < a_len - b_len + 1; i++){
             bool same = true;
             for(int j = 0; j < b_len; j++){
