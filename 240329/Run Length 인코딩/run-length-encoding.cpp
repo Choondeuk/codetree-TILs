487335c517fb72726935f2588dead8afe20ea200
이영원 <82967985+Choondeuk@users.noreply.github.com>
2024-03-29 16:43:55 +0900

    Title: Run Length 인코딩, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240329/Run Length 인코딩/run-length-encoding.cpp
+++ 240329/Run Length 인코딩/run-length-encoding.cpp
@@ -8,17 +8,19 @@ int main() {
     int len = a.length();
     int prev = 0;
     int cnt = 1;
-    for(int i = 1; i <= len; i++){
-        if(a[prev] == a[i]){
+    for(int i = 1; i < len; i++){
+        if(a[i-1] == a[i]){
             cnt++;
+            continue;
         }
         else    {
             b += to_string(cnt) + a[i];
             cnt = 1;
         }
-        prev = i;
+
         
     }
-    cout << b.length()-1 << endl << b;
+    b += to_string(cnt);
+    cout << b.length() << endl << b;
     return 0;
 }
