c4a62f10845eda2f48744a6e7d80babda1ab06ed
이영원 <82967985+Choondeuk@users.noreply.github.com>
2024-04-04 21:13:32 +0900

    Title: 빙빙 돌며 사각형 채우기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240404/빙빙 돌며 사각형 채우기/snail-alphabet-square.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+#define MAX 100
+int n, m;
+char a = 'A';
+int num = 0;
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, 1, 0, -1};
+int x = 0, y = 0;
+char arr[MAX][MAX] = {};
+
+bool Inrange(int y, int x){
+    return (0 <= y && y < n && 0 <= x && x < m);
+}
+
+void move(){
+    int nx = x + dx[num%4];
+    int ny = y + dy[num%4];
+    if(Inrange(ny, nx) == false || arr[ny][nx] != 0)
+        num++;
+    x += dx[num%4];
+    y += dy[num%4];
+}
+
+void print(){
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+int main() {
+    cin >> n >> m;
+    for(int i = 0; i < n*m;i++){
+        arr[y][x] = a;
+        move();
+        a++;    
+    }
+    print();
+    return 0;
+}
