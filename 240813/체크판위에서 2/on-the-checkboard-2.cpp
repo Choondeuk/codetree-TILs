6c36546ae26df0af1ae588726441b2a0975e572e
이영원 <82967985+Choondeuk@users.noreply.github.com>
2024-08-13 23:19:42 +0900

    Title: 체크판위에서 2, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240813/체크판위에서 2/on-the-checkboard-2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int R,C;
+    int cnt=0;
+    cin >>R>>C;
+    char A[R][C];
+    for(int i=0;i<R;i++){
+        for(int j=0;j<C;j++){
+            cin >>A[i][j]; 
+        }
+    }
+    char t =A[0][0];
+    for(int i=1;i<R;i++){
+        for(int j = 1;j<C-1;j++){
+            if (A[i][j] != t){
+                cnt = ((R-2)-i) *((C-2)-j);
+            }
+        }
+    }
+    cout << cnt;
+
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
