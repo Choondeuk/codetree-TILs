ee8085032d0f47edc3ed3d494d608d4f62ef98ee
이영원 <82967985+Choondeuk@users.noreply.github.com>
2024-08-20 08:24:33 +0900

    Title: 숨은 단어 찾기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240820/숨은 단어 찾기 2/find-hidden-words-2.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+#include <string>
+int main() {
+    int N,M;
+    cin >> N >>M;
+    string str[N];
+    for(int i=0;i<N;i++){
+        cin >> str[i];
+    }
+    int cnt=0;
+    for(int i=0;i<N;i++){
+        for(int j=0;j<M-2;j++){
+            if(str[i][j]=='L' && str[i][j+1]=='E' && str[i][j+2]=='E'){
+                cnt++;
+            }
+            else if(str[i][M-1-j]=='L' && str[i][M-2-j]=='E' && str[i][M-3-j]=='E'){
+                cnt++;
+            }
+        }
+    }
+    for(int i=0;i<N-2;i++){
+        for(int j=0;j<M;j++){
+            if(str[i][j]=='L' && str[i+1][j]=='E' && str[i+2][j]=='E'){
+                cnt++;
+            }
+            else if(str[N-1-i][j]=='L' && str[N-2-i][j]=='E' && str[N-2-i][j]=='E'){
+                cnt++;
+            }
+        }
+    }
+    for(int i=0;i<N-2;i++){
+        for(int j=0;j<M-2;j++){
+            if(str[i][j]=='L' && str[i+1][j+1]=='E' && str[i+2][j+2]=='E'){
+                cnt++;
+            }
+            else if(str[N-1-i][M-1-j]=='L' && str[N-2-i][M-2-j]=='E' && str[N-2-i][M-3-j]=='E'){
+                cnt++;
+            }
+        }
+    }
+    cout << cnt;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
