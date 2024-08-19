906c986d8a2684bb639d452cd4d356d91a6a78ca
이영원 <82967985+Choondeuk@users.noreply.github.com>
2024-08-20 07:17:52 +0900

    Title: Carry 피하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240820/Carry 피하기 2/escaping-carry-2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+#include <string>
+int main() {
+    int n;
+    cin >> n;
+    int a[n],max=-1,cnt=0;
+    string str[n];
+    for(int i=0;i<n;i++){
+        cin >>a[i];
+        str[i] = to_string(a[i]);
+    }
+
+    for(int i=0;i<n-2;i++){
+        for(int j=i+1;j<n-1;j++){
+            for(int k=j+1;k<n;k++){
+                if ((a[i]%10) +(a[j]%10) + (a[k]%10) <10){
+                    if ((a[i]%100/10) +(a[j]%100/10) + (a[k]%100/10) <10){
+                        if ((a[i]%1000/100) +(a[j]%1000/100) + (a[k]%1000/100) <10){
+                            if ((a[i]/1000) +(a[j]/1000) + (a[k]/1000) <10){
+                                cnt = a[i] + a[j] + a[k];
+                                if(cnt > max && cnt!=0){
+                                    max = cnt;
+                                }
+                            }
+                        }
+                    }
+                }
+                
+            }
+        }
+    }
+    cout << max;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
