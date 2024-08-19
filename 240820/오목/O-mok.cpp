64e79edef993c894120f0d421ea0d50bd8f95f06
이영원 <82967985+Choondeuk@users.noreply.github.com>
2024-08-20 08:00:31 +0900

    Title: 오목, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240820/오목/O-mok.cpp
+++ 240820/오목/O-mok.cpp
@@ -59,9 +59,59 @@ int main() {
             w=0;
         }
     }
+    for(int i=0;i<15;i++){
+        for(int j=0;j<15;j++){
+            for(int k=0;k<5;k++){
+                if (a[i+k][j+k]==1){
+                    b++;
+                    if(b==5){
+                        s=1;
+                        x=i+3;
+                        y=j+3;
+                    }
+                }
+                else if (a[i+k][j+k] == 2){
+                    w++;
+                    if(w==5){
+                        s=2;
+                        x=i+3;
+                        y=j+3;
+                    }
+                }
+            }
+            b=0;
+            w=0;
+        }
+    }
+    for(int i=0;i<15;i++){
+        for(int j=5;j<19;j++){
+            for(int k=0;k<5;k++){
+                if (a[i+k][j-k]==1){
+                    b++;
+                    if(b==5){
+                        s=1;
+                        x=i+3;
+                        y=j-2;
+                    }
+                }
+                else if (a[i+k][j-k] == 2){
+                    w++;
+                    if(w==5){
+                        s=2;
+                        x=i+3;
+                        y=j-2;
+                    }
+                }
+            }
+            b=0;
+            w=0;
+        }
+    }
     
     cout << s <<endl;
+    if(s!=0){
     cout << x <<" "<<y;
+    }
 
     // 여기에 코드를 작성해주세요.
     return 0;
