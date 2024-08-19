20b254c2319ad999f26921196bdb4a2baf72cbb7
이영원 <82967985+Choondeuk@users.noreply.github.com>
2024-08-20 08:33:40 +0900

    Title: 숨은 단어 찾기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240820/숨은 단어 찾기 2/find-hidden-words-2.cpp
+++ 240820/숨은 단어 찾기 2/find-hidden-words-2.cpp
@@ -24,7 +24,7 @@ int main() {
             if(str[i][j]=='L' && str[i+1][j]=='E' && str[i+2][j]=='E'){
                 cnt++;
             }
-            else if(str[N-1-i][j]=='L' && str[N-2-i][j]=='E' && str[N-2-i][j]=='E'){
+            else if(str[N-1-i][j]=='L' && str[N-2-i][j]=='E' && str[N-3-i][j]=='E'){
                 cnt++;
             }
         }
@@ -34,7 +34,21 @@ int main() {
             if(str[i][j]=='L' && str[i+1][j+1]=='E' && str[i+2][j+2]=='E'){
                 cnt++;
             }
-            else if(str[N-1-i][M-1-j]=='L' && str[N-2-i][M-2-j]=='E' && str[N-2-i][M-3-j]=='E'){
+            else if(str[N-1-i][M-1-j]=='L' && str[N-2-i][M-2-j]=='E' && str[N-3-i][M-3-j]=='E'){
+                cnt++;
+            }
+        }
+    }
+    for(int i=2;i<N;i++){
+        for(int j=0;j<M-2;j++){
+            if(str[i][j]=='L' && str[i-1][j+1]=='E' && str[i-2][j+2]=='E'){
+                cnt++;
+            }
+        }
+    }
+    for(int i=0;i<N-2;i++){
+        for(int j=2;j<M;j++){
+            if(str[i][j]=='L' && str[i+1][j-1]=='E' && str[i+2][j-2]=='E'){
                 cnt++;
             }
         }
