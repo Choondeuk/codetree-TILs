09f8d2cb6e2fd1254c2417e880c1e9782f2b09bb
이영원 <82967985+Choondeuk@users.noreply.github.com>
2024-08-20 12:48:50 +0900

    Title: G or H 3, Time: 79ms, Memory: 0MB, Status: Passed - CodeTree

--- 240820/G or H 3/G-or-H-3.cpp
+++ 240820/G or H 3/G-or-H-3.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 using namespace std;
-
+#define MAX 10000
 int main() {
     // 여기에 코드를 작성해주세요.
     int N,K;
     cin >> N >>K;
     int a[N];
     char b[N];
-    int c[10000]={0};
+    int c[MAX]={0};
     int max=0,cnt=0;
     int num=0;
 
@@ -23,8 +23,8 @@ int main() {
             c[a[i]-1] = 2;
         }
     }
-    for(int i =0;i<num-K+1;i++){
-        for(int j=0;j<=K;j++){
+    for(int i =0;i<MAX;i++){
+        for(int j=0;j<K+1&&i+j<10000;j++){
             cnt+=c[i+j];
         }
         if(cnt>max){
