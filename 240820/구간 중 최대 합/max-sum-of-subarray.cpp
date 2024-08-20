d6fb0f97fec90b9d6f76d2e05cbb1aa0b67520df
이영원 <82967985+Choondeuk@users.noreply.github.com>
2024-08-20 10:33:47 +0900

    Title: 구간 중 최대 합, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240820/구간 중 최대 합/max-sum-of-subarray.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n,k;
+    cin >> n >>k;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin >> a[i];
+    }
+    int max = 0,cnt = 0;
+    for(int i=0;i<n-k;i++){
+        for(int j=0;j<k;j++){
+            cnt +=a[i+j];
+        }
+        if(cnt > max){
+            max = cnt;
+            
+        }
+        cnt = 0;
+    }
+    cout << max;
+    return 0;
+}
