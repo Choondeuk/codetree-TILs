2c485d18e12d0cfdaa07155fc59d6f5fc2027e26
이영원 <82967985+Choondeuk@users.noreply.github.com>
2024-08-21 15:38:55 +0900

    Title: 바구니 안의 사탕 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240821/바구니 안의 사탕 2/candy-in-the-basket-2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int N,K;
+    cin >> N >> K;
+    int a[100]={0};
+    int cnt,loc;
+    for(int i=0;i<N;i++){
+        cin >> cnt >>loc;
+        a[loc+1] +=cnt;
+    }
+    int max = 0, cnt2=0,loc2;
+    for(int i=0;i<100-2*K;i++){
+        for(int j=0;j<2*K+1;j++){
+            cnt2 +=a[i+j];
+        }
+        if(cnt2>max){
+            max = cnt2;
+            loc2 = i+K-1;
+        }
+        cnt2 = 0;
+    }
+    cout <<max;
+    
+
+
+
+    
+    return 0;
+}
