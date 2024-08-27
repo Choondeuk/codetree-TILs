fcd6c6dce878f16db365f3a76548d137402e0ab2
이영원 <82967985+Choondeuk@users.noreply.github.com>
2024-08-27 16:53:20 +0900

    Title: 행복한 수열의 개수, Time: 11ms, Memory: 0MB, Status: Passed - CodeTree

--- 240827/행복한 수열의 개수/number-of-happy-sequence.cpp
+++ 240827/행복한 수열의 개수/number-of-happy-sequence.cpp
@@ -11,19 +11,23 @@ int main() {
             cin >>a[i][j]; 
         }
     }
-    int cnt1 =1,cnt2=0;
+    int cnt1 =1,cnt2=0,cnt3 = 1;
     for(int i=0;i<n;i++){
         for(int j=1;j<n;j++){
             if(a[i][j-1] == a[i][j]){
                 cnt1++;
             }
-            else if(a[i][j-1] != a[i][j]){
+            if(cnt1 > cnt3){
+                cnt3 = cnt1;
+            }
+            if(a[i][j-1] != a[i][j]){
                 cnt1 = 1;
             }
         }       
-        if(cnt1>=m){
+        if(cnt3>=m){
             cnt2++;
         }
+        cnt3 = 1;
         cnt1 = 1;
     }
     for(int j=0;j<n;j++){
@@ -31,13 +35,17 @@ int main() {
             if(a[i-1][j] == a[i][j]){
                 cnt1++;
             }
-            else if(a[i-1][j] != a[i][j]){
+            if(cnt1 > cnt3){
+                cnt3 = cnt1;
+            }
+            if(a[i-1][j] != a[i][j]){
                 cnt1 = 1;
             }
         }       
-        if(cnt1>=m){
+        if(cnt3>=m){
             cnt2++;
         }
+        cnt3 = 1;
         cnt1 = 1;
     }
     cout << cnt2;
